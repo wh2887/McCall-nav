637c7dee9ca53d169a8874bec6f60eda638b9aa8
wh2887 <13567272790@163.com>
2020-06-07 14:41:36 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,67 @@
+const $siteList = $(".siteList");
+const $lastLi = $siteList.find("li.last");
+const x = localStorage.getItem("x");
+const xObject = JSON.parse(x);
+const hasMap = xObject || [
+  { logo: "A", logoType: "text", url: "https://www.acfun.cn" },
+  { logo: "B", logoType: "image", url: "https://www.bilibili.com" },
+];
+const simplifyUrl = (url) => {
+  return url
+    .replace("https://", "")
+    .replace("http://", "")
+    .replace("www.", "")
+    .replace(/\/.*/, ""); //删除 / 开头的内容
+};
+const render = () => {
+  $siteList.find("li:not(.last)").remove();
+  hasMap.forEach((node, index) => {
+    const $li = $(`
+                  <li>
+                      <div class="logo">${node.logo}</div>
+                      <div class="link">${simplifyUrl(node.url)}</div>
+                      <div class='close'>
+                        <svg class="icon">
+                          <use xlink:href="#icon-searchclose"></use>
+                        </svg>
+                      </div>
+                  </li>`).insertBefore($lastLi);
+    $li.on("click", () => {
+      window.open(node.url);
+    });
+    $li.on("click", ".close", (e) => {
+      e.stopPropagation(); //阻止冒泡
+      hasMap.splice(index, 1);
+      render();
+    });
+  });
+};
+render();
+
+$(".addButton").on("click", () => {
+  let url = window.prompt("请问您要添加的网址是什么？");
+  if (url.indexOf("http") !== 0) {
+    url = "https://" + url;
+  }
+  hasMap.push({
+    logo: simplifyUrl(url)[0], //.toUpperCase()
+    logoType: "text",
+    url: url,
+  });
+  render();
+});
+
+window.onbeforeunload = () => {
+  const string = JSON.stringify(hasMap);
+  window.localStorage.setItem("x", string);
+};
+
+$(document).on("keypress", (e) => {
+  // const key = e.key;
+  const { key } = e; //上面一行代码的简写形式：如果发现你的变量名和后面的属性名是一样的额就可以这么写
+  for (let i = 0; i < hasMap.length; i++) {
+    if (hasMap[i].logo.toLowerCase() === key) {
+      window.open(hasMap[i].url);
+    }
+  }
+});
